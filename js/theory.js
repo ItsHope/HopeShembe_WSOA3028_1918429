3cc33a11b43e3e67b5f0f5cb7571c4c8a2ad2d40
ItsHope <1918429@students.wits.ac.za>
2021-06-17 21:49:37 +0200

    javascript

---
+++ js/theory.js
@@ -0,0 +1,17 @@
+const navigation = document.getElementById('navigation')
+const logo = document.getElementById('logo')
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        navigation.style.padding = "10px 5px";
+        logo.style.fontSize= "15px";
+
+    }
+
+    else {
+        navigation.style.padding = " 20px 10px";
+        logo.style.fontSize= "30px"
+    }
+}
