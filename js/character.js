85e0f6d9edc8dd09edb058940268a31d384825c7
ItsHope <1918429@students.wits.ac.za>
2021-06-18 14:38:05 +0200

    image viewer

--- js/character.js
+++ js/character.js
@@ -1,1 +1,18 @@
 let slider_img = document.querySelector(".slider-img");
+let images = ["1.jpj ", "2.jpg"];
+let i= 0;
+
+function prev(){
+  if(i<= 0 ) i = images.length;
+  i--;
+  return setImg();
+
+}
+
+function next(){
+
+}
+
+function setImg() {
+    return slider_img.setAttribute("src", "images"/+ images[i]);
+}
